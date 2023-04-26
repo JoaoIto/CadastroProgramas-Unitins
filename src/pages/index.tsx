72b2e9dd5ff70793c4ac9be50862a47fb6b3b193
João <joaovictorpfr@gmail.com>
2023-04-26 17:24:57 -0300

    add link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="h-screen w-screen flex flex-col items-center p-6">
-      <div id="texts" className="h-1/5 w-3/5 text-center">
+    <main className="h-screen w-screen font-inter flex flex-col items-center p-6">
+      <div id="texts" className="h-1/6 w-3/5 text-center flex flex-col justify-around">
         <h1 className="text-4xl">Cadastro de Programas de Computadores</h1>
         <p>
           Este é o serviço de cadastro e consulta para a propriedade de
@@ -11,12 +13,14 @@ export default function Home() {
       </div>
 
       <div id="btns" className="h-40 flex flex-col justify-around">
-        <button className="h-16 w-40 text-white bg-blue-800 rounded-md border-2 border-solid border-zinc-700">
+        <button className="h-16 w-40 text-white font-bold bg-blue-800 rounded-md border-2 border-solid border-zinc-700">
           CADASTRAR PROPRIEDADE
         </button>
-        <button className="h-16 w-40 text-white bg-blue-800 rounded-md border-2 border-solid border-zinc-700">
+        <Link href="./cadastro">
+        <button className="h-16 w-40 text-white font-bold bg-blue-800 rounded-md border-2 border-solid border-zinc-700">
           CONSULTAR PROPRIEDADE
         </button>
+        </Link>
       </div>
     </main>
   );
