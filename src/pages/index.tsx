7d64cdcab6204e1cd122df7f54f3754bea5ad862
João <joaovictorpfr@gmail.com>
2023-04-26 14:54:49 -0300

    form base react hook form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import { Form } from '../components/Form'
+
 export default function Home() {
   return (
     <main
-      className="">
+      className="h-3/5 w-screen flex items-center">
       <h1>Hello World!</h1>
+
+      <Form/>
     </main>
   )
 }
