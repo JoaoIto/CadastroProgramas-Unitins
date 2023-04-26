364c44888fc28214e8bfdc242dab6a5a67c98098
João <joaovictorpfr@gmail.com>
2023-04-26 18:00:40 -0300

    config routhes next and react

--- web/next/next.config.js
+++ web/next/next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/:path*",
+      },
+      {
+        source: "/:path*",
+        destination: "http://localhost:5173/:path*",
+      },
+      {
+        source: "/",
+        destination: "http://localhost:5173/",
+      },
+    ];
+  }
 }
 
 module.exports = nextConfig
