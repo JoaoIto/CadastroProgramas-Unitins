d91ec28ee367b6a8a9b6fd46cbcf01af78bf7d43
João <joaovictorpfr@gmail.com>
2023-07-20 15:35:31 -0300

    base backend usuario-programa

--- server/src/usuario-programa/usuario-programa.module.ts
+++ server/src/usuario-programa/usuario-programa.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import {MongooseModule} from "@nestjs/mongoose";
-import {UsuarioPrograma, UsuarioProgramaSchema} from "./usuario-programa.model";
+import {UsuarioProgramaSchema} from "./usuario-programa.model";
 import {UsuarioProgramaController} from "./usuario-programa.controller";
 import {UsuarioProgramaService} from "./usuario-programa.service";
 import {UsuarioProgramaRepository} from "./usuario-programa.repository";
 @Module({imports: [
-        MongooseModule.forFeature([{ name: 'usuario-programa', schema: UsuarioProgramaSchema }]),
+        MongooseModule.forFeature([{ name: 'UsuarioPrograma', schema: UsuarioProgramaSchema }]),
     ],
     controllers: [UsuarioProgramaController],
     providers: [UsuarioProgramaService, UsuarioProgramaRepository],
