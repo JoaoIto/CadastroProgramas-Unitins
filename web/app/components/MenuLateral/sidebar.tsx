aca5af187840e2ed8b5f615f1f274bf7fa889408
JoaoIto <joaovictorpfr@gmail.com>
2023-09-17 18:53:18 -0300

    add styles for cadastrar page, and styles add in sidebar, buttons

--- web/app/components/MenuLateral/sidebar.tsx
+++ web/app/components/MenuLateral/sidebar.tsx
@@ -9,7 +9,8 @@ export const Sidebar = () => {
     const isSmallScreen = window.innerWidth < 870;
 
     return (
-        <aside className={`w-full w-[300px] bg-blue-900 text-white flex flex-col items-center border-r-4 border-b-4 border-slate-700`}>
+        <aside style={{ maxWidth: isSmallScreen ? '120px' : 'none' }} // Aplicar largura máxima em uma visualização móvel
+            className={`w-[300px] bg-blue-900 text-white flex flex-col items-center border-r-4 border-b-4 border-slate-700`}>
             {/* Logo */}
             {!isSmallScreen && <Image src={UnitinsLogo} alt="Unitins Logo" className="h-56 w-full"/>}
             {/* Conteúdo da barra lateral */}
@@ -17,19 +18,19 @@ export const Sidebar = () => {
                 <li className="flex items-center space-x-2 p-2">
                     <ButtonLinkPage href="/dashboard">
                         <HomeIcon className="h-10 w-10" />
-                        {!isSmallScreen && <h3 className="font-bold text-lg p-4">Inicial</h3>}
+                        {!isSmallScreen && <h3 className="font-medium text-lg p-4">Inicial</h3>}
                     </ButtonLinkPage>
                 </li>
                 <li className="flex items-center space-x-2 p-2">
                     <ButtonLinkPage href="/programa/listar">
                         <AccountCircleIcon className="h-10 w-10" />
-                        {!isSmallScreen && <h3 className="font-bold text-lg p-4">Programas</h3>}
+                        {!isSmallScreen && <h3 className="font-medium text-lg p-4">Programas</h3>}
                     </ButtonLinkPage>
                 </li>
                 <li className="flex items-center space-x-2 p-2">
                     <ButtonLinkPage href="/perfil">
                         <AccountCircleIcon className="h-10 w-10" />
-                        {!isSmallScreen && <h3 className="font-bold text-lg p-4">Perfil</h3>}
+                        {!isSmallScreen && <h3 className="font-medium text-lg p-4">Perfil</h3>}
                     </ButtonLinkPage>
                 </li>
                 {/* O restante do conteúdo da barra lateral aqui */}
--- web/app/programa/cadastrar/page.tsx
+++ web/app/programa/cadastrar/page.tsx
@@ -13,169 +13,182 @@ import {Sidebar} from '../../components/MenuLateral/sidebar';
 import ApiUtils from '@/app/Utils/Api/apiMethods';
 import {ProgramaStatus} from '@/app/enum/programa-status.enum';
 import Grid from "@mui/material/Grid";
+import Title from "@/app/components/Title/title";
 
 const programa = z.object({
-  nomeCompleto: z.string().nonempty('Campo obrigatório'),
-  rg: z.string().refine(value => /^\d+$/.test(value), {
-    message: 'Somente números',
-    path: ['rg'],
-  }).refine(value => value.length >= 7, {
-    message: 'Mínimo de 7 dígitos',
-    path: ['rg'],
-  }),
-  cpf: z.string().refine(value => /^\d+$/.test(value), {
-    message: 'Somente números',
-    path: ['cpf'],
-  }).refine(value => value.length >= 11, {
-    message: 'Mínimo de 11 dígitos',
-    path: ['cpf'],
-  }),
-  dataNascimento: z.string().nonempty('Campo obrigatório'),
-  estadoCivil: z.string().nonempty('Campo obrigatório'),
-  status: z.string(),
-  nomeArquivo: z.any(),
+    nomeCompleto: z.string().nonempty('Campo obrigatório'),
+    rg: z.string().refine(value => /^\d+$/.test(value), {
+        message: 'Somente números',
+        path: ['rg'],
+    }).refine(value => value.length >= 7, {
+        message: 'Mínimo de 7 dígitos',
+        path: ['rg'],
+    }),
+    cpf: z.string().refine(value => /^\d+$/.test(value), {
+        message: 'Somente números',
+        path: ['cpf'],
+    }).refine(value => value.length >= 11, {
+        message: 'Mínimo de 11 dígitos',
+        path: ['cpf'],
+    }),
+    dataNascimento: z.string().nonempty('Campo obrigatório'),
+    estadoCivil: z.string().nonempty('Campo obrigatório'),
+    status: z.string(),
+    nomeArquivo: z.any(),
 });
 
 type FormData = z.infer<typeof programa>;
 
 function NovaSolicitacao() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>({
-    resolver: zodResolver(programa),
-  });
+    const {
+        register,
+        handleSubmit,
+        formState: {errors},
+    } = useForm<FormData>({
+        resolver: zodResolver(programa),
+    });
 
-  const usuarioId = sessionStorage.getItem('perfilId');
-  let fileName: string | undefined = undefined;
-  const [status, setStatus] = useState<ProgramaStatus>(ProgramaStatus.RASCUNHO);
+    const usuarioId = sessionStorage.getItem('perfilId');
+    let fileName: string | undefined = undefined;
+    const [status, setStatus] = useState<ProgramaStatus>(ProgramaStatus.RASCUNHO);
 
-  console.log("Render")
-  // Função para enviar o arquivo para /programa/uploads
-  async function enviarArquivo(arquivo: File) {
-    try {
-      const formData = new FormData();
-      formData.append('file', arquivo);
-      const response = await ApiUtils.post('http://localhost:3333/programa/uploads', formData);
-      return response;
-    } catch (error) {
-      console.error('Erro ao enviar o arquivo:', error);
-      throw error;
+    console.log("Render")
+
+    // Função para enviar o arquivo para /programa/uploads
+    async function enviarArquivo(arquivo: File) {
+        try {
+            const formData = new FormData();
+            formData.append('file', arquivo);
+            const response = await ApiUtils.post('http://localhost:3333/programa/uploads', formData);
+            return response;
+        } catch (error) {
+            console.error('Erro ao enviar o arquivo:', error);
+            throw error;
+        }
     }
-  }
 
-  const onSubmit = async (data: FormData) => {
-    try {
-      if (fileName) {
-        const response = await enviarArquivo(data.nomeArquivo[0]);
-      }
-      data.nomeArquivo = data.nomeArquivo[0].name;
-      console.log(data);
-      // Enviar o formulário para /programa/cadastrar
-      const programaCriado = await ApiUtils.post('http://localhost:3333/programa/cadastrar', {
-        ...data,
-        status: status,
-      });
-      // window.open('/dashboard', '_self'); // Abre a página de dashboard na mesma janela
-    } catch (error) {
-      console.error('Erro ao cadastrar o programa:', error);
-    }
-  };
+    const onSubmit = async (data: FormData) => {
+        try {
+            if (fileName) {
+                const response = await enviarArquivo(data.nomeArquivo[0]);
+            }
+            data.nomeArquivo = data.nomeArquivo[0].name;
+            console.log(data);
+            // Enviar o formulário para /programa/cadastrar
+            const programaCriado = await ApiUtils.post('http://localhost:3333/programa/cadastrar', {
+                ...data,
+                status: status,
+            });
+            // window.open('/dashboard', '_self'); // Abre a página de dashboard na mesma janela
+        } catch (error) {
+            console.error('Erro ao cadastrar o programa:', error);
+        }
+    };
 
-  const handleSave = () => {
-    console.log("Salvar clickado!")
-    setStatus(ProgramaStatus.RASCUNHO); // Altera o status para RASCUNHO antes de chamar a função de submissão
-    handleSubmit(onSubmit); // Chama a função de submissão passando os dados do formulário
-  };
-  const handleSend = () => {
-    console.log("Enviar clickado!")
-    setStatus(ProgramaStatus.ENVIADO); // Altera o status para ENVIADO antes de chamar a função de submissão
-    handleSubmit(onSubmit); // Chama a função de submissão passando os dados do formulário
-  };
+    const handleSave = () => {
+        console.log("Salvar clickado!")
+        setStatus(ProgramaStatus.RASCUNHO); // Altera o status para RASCUNHO antes de chamar a função de submissão
+        handleSubmit(onSubmit); // Chama a função de submissão passando os dados do formulário
+    };
+    const handleSend = () => {
+        console.log("Enviar clickado!")
+        setStatus(ProgramaStatus.ENVIADO); // Altera o status para ENVIADO antes de chamar a função de submissão
+        handleSubmit(onSubmit); // Chama a função de submissão passando os dados do formulário
+    };
 
-  return <div className="flex h-screen">
-    <Sidebar />
-    <div className="flex-grow p-8">
-      <h1 className="text-3xl font-bold mb-4 text-center">Nova Solicitação</h1>
-      <form className="max-w-md mx-auto" onSubmit={handleSubmit(onSubmit)}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={6}>
-            <TextField
-                label="Nome Completo"
-                {...register('nomeCompleto')}
-                fullWidth
-                type="text"
-                error={!!errors.nomeCompleto}
-                helperText={errors.nomeCompleto?.message || ' '}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-                label="RG"
-                {...register('rg')}
-                fullWidth
-                type="text"
-                error={!!errors.rg}
-                helperText={errors.rg?.message || ' '}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-                label="CPF"
-                {...register('cpf')}
-                fullWidth
-                type="text"
-                error={!!errors.cpf}
-                helperText={errors.cpf?.message || ' '}
-            />
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <TextField
-                label="Data de Nascimento"
-                type="date"
-                {...register('dataNascimento')}
-                fullWidth
-                error={!!errors.dataNascimento}
-                helperText={errors.dataNascimento?.message || ' '}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl fullWidth error={!!errors.estadoCivil}>
-              <InputLabel htmlFor="estadoCivil">Estado Civil</InputLabel>
-              <Select id="estadoCivil" {...register('estadoCivil')}>
-                <MenuItem value="solteiro">Solteiro</MenuItem>
-                <MenuItem value="casado">Casado</MenuItem>
-                <MenuItem value="viuvo">Viúvo</MenuItem>
-              </Select>
-              {errors.estadoCivil && <span className="text-red-500">{errors.estadoCivil.message}</span>}
-            </FormControl>
-          </Grid>
-          <Grid>
-            <input
-                type="file"
-                accept=".pdf,.doc,.docx, .json, .zip, .java, .py"
-                {...register('nomeArquivo')}
-                onChange={(event) => {
-                  if (event.target.files && event.target.files.length > 0) {
-                    fileName = event.target.files[0].name;
-                  }
-                }}
-            />
-          </Grid>
-        </Grid>
+    const isSmallScreen = window.innerWidth < 870;
 
-        <div className="mt-4">
-          <button type="submit" onClick={handleSave}>
-            Salvar
-          </button>
-          <button type="submit" onClick={handleSend}>
-            Enviar
-          </button>
+    return <div className="flex w-screen h-screen">
+        <Sidebar/>
+        <div className="flex-grow bg-sky-200 p-8">
+            <Title>Nova Solicitação</Title>
+            <form className="mx-auto" onSubmit={handleSubmit(onSubmit)}>
+                <Grid
+                    style={{ maxWidth: isSmallScreen ? '400px' : 'none' }} // Aplicar largura máxima em uma visualização móvel
+                    className="bg-white p-4 border-4 border-l-[10px] border-t-[10px] border-l-blue-300 border-t-blue-300 rounded-xl m-0"
+                    container spacing={2}>
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            label="Nome Completo"
+                            {...register('nomeCompleto')}
+                            fullWidth
+                            type="text"
+                            error={!!errors.nomeCompleto}
+                            helperText={errors.nomeCompleto?.message || ' '}
+                            style={{ maxWidth: isSmallScreen ? '220px' : 'none' }} // Aplicar largura máxima em uma visualização móvel
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            label="RG"
+                            {...register('rg')}
+                            fullWidth
+                            type="text"
+                            error={!!errors.rg}
+                            helperText={errors.rg?.message || ' '}
+                            style={{ maxWidth: isSmallScreen ? '220px' : 'none' }} // Aplicar largura máxima em uma visualização móvel
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            label="CPF"
+                            {...register('cpf')}
+                            fullWidth
+                            type="text"
+                            error={!!errors.cpf}
+                            helperText={errors.cpf?.message || ' '}
+                            style={{ maxWidth: isSmallScreen ? '220px' : 'none' }} // Aplicar largura máxima em uma visualização móvel
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <TextField
+                            label="Data de Nascimento"
+                            type="date"
+                            {...register('dataNascimento')}
+                            fullWidth
+                            error={!!errors.dataNascimento}
+                            helperText={errors.dataNascimento?.message || ' '}
+                            style={{ maxWidth: isSmallScreen ? '220px' : 'none' }} // Aplicar largura máxima em uma visualização móvel
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <FormControl fullWidth error={!!errors.estadoCivil}>
+                            <InputLabel htmlFor="estadoCivil">Estado Civil</InputLabel>
+                            <Select id="estadoCivil" {...register('estadoCivil')}>
+                                <MenuItem value="solteiro">Solteiro</MenuItem>
+                                <MenuItem value="casado">Casado</MenuItem>
+                                <MenuItem value="viuvo">Viúvo</MenuItem>
+                            </Select>
+                            {errors.estadoCivil && <span className="text-red-500">{errors.estadoCivil.message}</span>}
+                        </FormControl>
+                    </Grid>
+                    <Grid>
+                        <input
+                            type="file"
+                            accept=".pdf,.doc,.docx, .json, .zip, .java, .py"
+                            {...register('nomeArquivo')}
+                            onChange={(event) => {
+                                if (event.target.files && event.target.files.length > 0) {
+                                    fileName = event.target.files[0].name;
+                                }
+                            }}
+                        />
+                    </Grid>
+                </Grid>
+
+                <div className="mt-4">
+                    <button className="bg-blue-500 border-solid border-2 border-slate-100 text-white font-medium p-2 px-4 rounded-md mx-2" type="submit"
+                            onClick={handleSave}>
+                        Salvar
+                    </button>
+                    <button className="bg-blue-700 border-solid border-2 border-slate-100 text-white font-medium p-2 px-4 rounded-md mx-2" type="submit"
+                            onClick={handleSend}>
+                        Enviar
+                    </button>
+                </div>
+            </form>
         </div>
-      </form>
-    </div>
-  </div>;
+    </div>;
 }
 
 export default NovaSolicitacao;
