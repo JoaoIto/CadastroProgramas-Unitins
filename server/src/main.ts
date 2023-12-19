b548bf9be850e47c0db9c98bb10d2e793e86f017
JoaoIto <joaovictorpfr@gmail.com>
2023-12-19 17:27:03 -0300

    criando config de swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
-  await app.listen(3333);
+  const config = new DocumentBuilder()
+    .setTitle('Software_Hub-API')
+    .setDescription('The Software_Hub-API description')
+    .setVersion('1.0')
+    .addTag('Unitins-PI')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(8080);
   console.log('Conexão estabelecida');
 }
 bootstrap();
