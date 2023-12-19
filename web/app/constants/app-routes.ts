7fec1ad9a9a9cf28c9f7d175eb9b571abbf7b507
JoaoIto <joaovictorpfr@gmail.com>
2023-12-19 15:36:32 -0300

    criando rotas privadas

---
+++ web/app/constants/app-routes.ts
@@ -0,0 +1,11 @@
+export const APP_ROUTES = {
+    private: {
+        dashboard: '/',
+        cadastrar: '/programa/cadastrar',
+        editar: '/programa/editar',
+        listar: '/programa/listar'
+    },
+    public: {
+        login: 'auth/login'
+    }
+}
