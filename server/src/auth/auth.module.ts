99c8453f1cbd4e557d1560f1690619fa98ca1fbd
JoaoIto <joaovictorpfr@gmail.com>
2023-12-20 16:06:17 -0300

    fazendo importacao de modulos necessarios

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from "./auth.controller";
 import { HashService } from 'src/hash/hash.service';
-import { JwtModule } from '@nestjs/jwt';
+import { JwtModule, JwtService } from "@nestjs/jwt";
+import { UsuarioService } from "../usuario/usuario.service";
+import { UsuarioRepository } from "../usuario/usuario.repository";
+import { UsuarioModule } from "../usuario/usuario.module";
+import { AuthService } from "./auth.service";
 
 @Module({
     imports: [
         JwtModule.register({
             secret: 'software_hub-unitins', // Substitua com a sua chave secreta
             signOptions: { expiresIn: '1h' }, // Configurações adicionais, se necessário
-          })
+          }),
+      UsuarioModule
     ],
     controllers: [AuthController],
-    providers: [HashService]
+    providers: [AuthService, HashService, JwtService]
 })
 
 export class AuthModule { }
