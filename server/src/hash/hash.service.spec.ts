683ebe458287161f22b12383a1f0d25186aa3fa7
JoaoIto <joaovictorpfr@gmail.com>
2023-12-20 03:32:19 -0300

    fazendo hash de senha e testes passaram

---
+++ server/src/hash/hash.service.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { HashService } from './hash.service';
+
+describe('HashService', () => {
+  let hashService: HashService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [HashService],
+    }).compile();
+
+    hashService = module.get<HashService>(HashService);
+  });
+
+  it('deve gerar e verificar o hash corretamente', async () => {
+    const senhaOriginal = 'senha123';
+
+    // Gera o hash
+    const hashSenha = await hashService.getHashSenha(senhaOriginal);
+
+    // Verifica se o hash é válido
+    const verificaHash = await hashService.verifyHashSenha(senhaOriginal, hashSenha);
+
+    expect(verificaHash).toBe(true);
+  });
+});
--- server/src/hash/hash.service.ts
+++ server/src/hash/hash.service.ts
@@ -1,16 +1,21 @@
-// import { Injectable } from '@nestjs/common';
-// import { genSalt, hash, compare } from 'bcryptjs';
-//
-// @Injectable()
-// export class HashService {
-//   async getHashSenha(senha: string): Promise<string> {
-//     const salt = await genSalt();
-//     const hash = await hash(senha, salt);
-//
-//     return hash;
-//   }
-//
-//   async verifyHashSenha(senha: string, hash: string): Promise<boolean> {
-//     return await compare(senha, hash);
-//   }
-// }
+import { Injectable } from '@nestjs/common';
+import { genSalt, hash as bcryptHash, compare } from 'bcryptjs';
+
+@Injectable()
+export class HashService {
+  async getHashSenha(senha: string): Promise<string> {
+    const salt = await genSalt();
+    const hashedSenha = await bcryptHash(senha, salt);
+
+    return hashedSenha;
+  }
+
+  async verifyHashSenha(senha: string, hashedSenha: string): Promise<boolean> {
+    return await compare(senha, hashedSenha);
+  }
+}
+
+const senha = "senha1"
+const hashService = new HashService();
+const senhaHash = hashService.getHashSenha(senha)
+console.log(senha, senhaHash)
