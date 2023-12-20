83aad6b09cf417e72fca758a303580d944267236
JoaoIto <joaovictorpfr@gmail.com>
2023-12-19 22:56:05 -0300

    criando log de programa

--- server/src/programa/programa.repository.ts
+++ server/src/programa/programa.repository.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@nestjs/common';
+import { Injectable, Logger } from "@nestjs/common";
 import {Programa} from "./programa.model";
 import mongoose, {Model} from "mongoose";
 import {InjectModel} from "@nestjs/mongoose";
@@ -6,6 +6,7 @@ import {AtualizarProgramaDto} from "./dto/atualizarPrograma.dto";
 
 @Injectable()
 export class ProgramaRepository {
+    private readonly logger = new Logger(ProgramaRepository.name);
     constructor(
         @InjectModel(Programa.name) private readonly programa: Model<Programa>
     ) {}
@@ -23,7 +24,9 @@ export class ProgramaRepository {
     }
 
     async findById(uuid): Promise<Programa> {
-        return this.programa.findById(uuid).exec();
+        const programa = await this.programa.findById(uuid).exec();
+        this.logger.log('programa retornado: ' + programa);
+        return programa;
     }
 
     async update(
@@ -35,12 +38,12 @@ export class ProgramaRepository {
             updateData,
             { new: true },
         );
-        console.log(`O programa foi atualizado! Programa atualizado ${uuid}: ${programaAtualizado}`)
+        this.logger.log(`O programa foi atualizado! Programa atualizado ${uuid}: ${programaAtualizado}`)
         return programaAtualizado;
     }
 
     async delete(uuid: string): Promise<void> {
         const programaApagar = await this.programa.findByIdAndDelete(uuid);
-        console.log(`O programa foi deletado! Programa deletado ${uuid}: ${programaApagar}`);
+        this.logger.log(`O programa foi deletado! Programa deletado ${uuid}: ${programaApagar}`);
     }
 }
