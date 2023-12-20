402d785207d50b567406ea97f843aaf0abc85abd
JoaoIto <joaovictorpfr@gmail.com>
2023-12-20 04:17:11 -0300

    adicionando a dependencia ao modulo principal

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,11 +5,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import {ProgramaModule} from "./programa/programa.module";
 import {UsuarioModule} from "./usuario/usuario.module";
 import {UsuarioProgramaModule} from "./usuario-programa/usuario-programa.module";
+import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [
       ProgramaModule,
       UsuarioModule,
       UsuarioProgramaModule,
+      AuthModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/softwarehub'),
   ],
   controllers: [],
