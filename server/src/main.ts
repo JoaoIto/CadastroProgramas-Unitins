28bc0ace76ddab1fe06d4d163d46cd12430b42bb
JoaoIto <joaovictorpfr@gmail.com>
2023-12-19 22:32:15 -0300

    usando log

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,8 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { Logger } from "@nestjs/common";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger();
+  app.useLogger(logger);
   app.use(cors());
   const config = new DocumentBuilder()
     .setTitle('Software_Hub-API')
