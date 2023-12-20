a307c23504275fc1ace45d2b2863cb8fa0fa84ed
JoaoIto <joaovictorpfr@gmail.com>
2023-12-20 16:11:08 -0300

    gerando logs para informacoes de service e controller de auth

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from "@nestjs/common";
 import { LoginDTO } from 'src/usuario/dto/login.dto';
 import {
   ApiBody,
@@ -11,6 +11,7 @@ import { AuthService } from './auth.service';
 @ApiTags('auth')
 @Controller('/auth')
 export class AuthController {
+  private readonly logger = new Logger(AuthController.name);
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
@@ -18,6 +19,7 @@ export class AuthController {
   @ApiBody({ type: LoginDTO, description: 'Credenciais do usuário para login' })
   @ApiResponse({ status: 200, description: 'Token gerado com sucesso' })
   async generateToken(@Body() loginDTO: LoginDTO): Promise<{ access_token: string }> {
+    this.logger.log(`Tentando autenticação a partir do usuario com cpf: ${loginDTO.cpf}; e senha: ${loginDTO.senha}`)
     const token = await this.authService.signIn(loginDTO);
     return token;
   }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException } from "@nestjs/common";
 import { UsuarioService } from '../usuario/usuario.service';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDTO } from "../usuario/dto/login.dto";
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
   constructor(
     private readonly usuarioService: UsuarioService,
     private readonly jwtService: JwtService
@@ -14,11 +15,13 @@ export class AuthService {
     const usuario = await this.usuarioService.login(loginDTO);
 
     if (usuario) {
+      this.logger.log("Usuario com credenciais encontrado! Gerando token...")
       const payload = { cpf: usuario.cpf, senha: usuario.senha };
       const access_token = await this.jwtService.signAsync(payload, {
-        secret: 'software_hub-unitins', // Substitua com a sua chave secreta
+        secret: 'software_hub-unitins',
         expiresIn: '1h',
       });
+      this.logger.log(`Token de acesso gerado: ${access_token}`)
       return { access_token };
     } else {
       throw new UnauthorizedException();
