f816090096ce7ff7c9d1208b430ddf504fad493c
JoaoIto <joaovictorpfr@gmail.com>
2023-12-20 15:32:59 -0300

    construindo modelo correto de usuario

--- server/src/usuario/usuario.model.ts
+++ server/src/usuario/usuario.model.ts
@@ -1,49 +1,26 @@
-import {Prop, Schema, SchemaFactory} from '@nestjs/mongoose';
-import mongoose, {Document, SchemaTypes} from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+import { ApiProperty } from "@nestjs/swagger";
 
 export type UsuarioDocument = Usuario & Document;
 
 @Schema({ collection: 'usuario' })
 export class Usuario {
-    @Prop({type: SchemaTypes.ObjectId, required: true}) private _id: mongoose.Types.ObjectId;
-
-    @Prop({ required: true }) private _perfil: string;
-
-    @Prop({ required: true }) private _nome: string;
-
-    @Prop({ required: true }) private _cpf: string;
+    @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
+    @ApiProperty()
+     _id: mongoose.Types.ObjectId;
 
     @Prop({ required: true })
-    senha: string;
+     perfil: string;
 
+    @Prop({ required: true })
+     nome: string;
 
-    get id(): mongoose.Types.ObjectId {
-        return this._id;
-    }
+    @Prop({ required: true })
+     cpf: string;
 
-    get perfil(): string {
-        return this._perfil;
-    }
-
-    set perfil(value: string) {
-        this._perfil = value;
-    }
-
-    get nome(): string {
-        return this._nome;
-    }
-
-    set nome(value: string) {
-        this._nome = value;
-    }
-
-    get cpf(): string {
-        return this._cpf;
-    }
-
-    set cpf(value: string) {
-        this._cpf = value;
-    }
+    @Prop({ required: true })
+     senha: string;
 }
 
 export const UsuarioSchema = SchemaFactory.createForClass(Usuario);
