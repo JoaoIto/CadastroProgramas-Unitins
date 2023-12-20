8c3f1cc5f2550641f8ac3d6799fad261108ac74d
JoaoIto <joaovictorpfr@gmail.com>
2023-12-19 22:15:51 -0300

    gerando get and setters no model de usuario

--- server/src/usuario/usuario.model.ts
+++ server/src/usuario/usuario.model.ts
@@ -5,20 +5,45 @@ export type UsuarioDocument = Usuario & Document;
 
 @Schema({ collection: 'usuario' })
 export class Usuario {
-    @Prop({type: SchemaTypes.ObjectId, required: true})
-    _id: mongoose.Types.ObjectId;
+    @Prop({type: SchemaTypes.ObjectId, required: true}) private _id: mongoose.Types.ObjectId;
 
-    @Prop({ required: true })
-    perfil: string;
+    @Prop({ required: true }) private _perfil: string;
 
-    @Prop({ required: true })
-    nome: string;
+    @Prop({ required: true }) private _nome: string;
 
-    @Prop({ required: true })
-    cpf: string;
+    @Prop({ required: true }) private _cpf: string;
 
     // @Prop({ required: true })
     // senha: string;
+
+
+    get id(): mongoose.Types.ObjectId {
+        return this._id;
+    }
+
+    get perfil(): string {
+        return this._perfil;
+    }
+
+    set perfil(value: string) {
+        this._perfil = value;
+    }
+
+    get nome(): string {
+        return this._nome;
+    }
+
+    set nome(value: string) {
+        this._nome = value;
+    }
+
+    get cpf(): string {
+        return this._cpf;
+    }
+
+    set cpf(value: string) {
+        this._cpf = value;
+    }
 }
 
 export const UsuarioSchema = SchemaFactory.createForClass(Usuario);
