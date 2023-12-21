72aca27d575a21ece024e0bdf3d5946fbac200a3
JoaoIto <joaovictorpfr@gmail.com>
2023-12-20 21:33:40 -0300

    jwt strategy padrao

---
+++ server/src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { jwtConstants } from './constans';
+import { LoginDTO } from "../usuario/dto/login.dto";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  async validate(payload: any) {
+    return { cpf: payload.cpf, senha: payload.senha, perfil: payload.perfil };
+  }
+}
