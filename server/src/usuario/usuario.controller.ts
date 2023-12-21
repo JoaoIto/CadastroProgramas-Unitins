fb1dbaecdf1030e17e061bc7b57cf5a65ccc7033
JoaoIto <joaovictorpfr@gmail.com>
2023-12-20 21:38:08 -0300

    colocando usuarios a serem aceitos para controllers

--- server/src/usuario/usuario.controller.ts
+++ server/src/usuario/usuario.controller.ts
@@ -1,20 +1,31 @@
-import { Controller, Post, Body, Get, Param, Put, Delete, Logger } from "@nestjs/common";
-import {UsuarioService} from "./usuario.service";
-import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { Body, Controller, Get, Logger, Param, Post, Req, UnauthorizedException, UseGuards } from "@nestjs/common";
+import { UsuarioService } from "./usuario.service";
+import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { LoginDTO } from "./dto/login.dto";
+import { Role } from "../roles/roles.enum";
+import { Roles } from "../roles/roles.decorator";
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+import { RolesGuard } from "../roles/roles.guard";
+
 @ApiTags('usuario')
 @Controller('/usuario')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
     export class UsuarioController{
     private readonly logger = new Logger(UsuarioController.name);
 
     constructor (private usuarioService: UsuarioService){}
     @Get()
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @ApiBearerAuth()
+    @Roles(Role.Admin)
     @ApiOperation({ summary: 'Faz a busca dos dados de todos os usuários' })
-    getDados(){
-        this.logger.log('Fazendo a busca dos dados de todos os usuários')
-        return this.usuarioService.listar()
+    async getDados() {
+            return this.usuarioService.listar();
     }
 
+    @ApiBearerAuth()
+    @Roles(Role.Admin)
     @ApiOperation({ summary: 'Faz a busca dos dados do usuario pelo uuid dele' })
     @Get('/:uuid')
     consultar(@Param("uuid") uuid: string) {
@@ -22,17 +33,20 @@ import { LoginDTO } from "./dto/login.dto";
         return this.usuarioService.consultar(uuid);
     }
 
+    @ApiBearerAuth()
+    @Roles(Role.Admin)
     @ApiOperation({ summary: 'Faz a busca dos dados do usuario pelo cpf dele' })
     @Get('/cpf/:cpf')
     consultarByCpf(@Param("cpf") cpf: string) {
         this.logger.log('Fazendo a busca dos dados do usuario com o cpf: ' + cpf)
         return this.usuarioService.consultarByCpf(cpf);
     }
+    @ApiBearerAuth()
+    @Roles(Role.Admin, Role.User)
     @ApiOperation({ summary: 'Faz a busca dos dados do usuario pelo login dele (cpf e senha)' })
     @Post('/login')
     login(@Body() loginDTO: LoginDTO) {
         this.logger.log(`Fazendo a busca dos dados do usuario com o login: ${loginDTO.cpf} e senha: ${loginDTO.senha}`)
         return this.usuarioService.login(loginDTO);
     }
-
 }
