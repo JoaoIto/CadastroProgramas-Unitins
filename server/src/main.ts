085a074bc05ea3a423f68b01b9b4a7396ae199d4
JoaoIto <joaovictorpfr@gmail.com>
2023-12-20 23:37:25 -0300

    log start

--- server/src/main.ts
+++ server/src/main.ts
@@ -16,6 +16,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   await app.listen(8080);
-  console.log('Conexão estabelecida');
+  logger.log("Conexão estabelecida com o BD!")
 }
 bootstrap();
