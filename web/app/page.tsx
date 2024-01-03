b50548338b4de0097e0ae110e9caa02a96426b22
JoaoIto <joaovictorpfr@gmail.com>
2024-01-03 02:34:50 -0300

    rota raiz = dashboard

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,10 +1,147 @@
-import SignIn from ".";
+"use client";
+import React, { useEffect, useState } from "react";
+import { CardProgram } from "./components/CardPrograma/Card";
+import ButtonLinkPage from "./components/ButtonLinkPage/ButtonLinkPage";
+import ApiUtils from "@/app/Utils/Api/apiMethods";
+import { Sidebar } from "@/app/components/MenuLateral/sidebar";
+import { Cabecalho } from "@/app/components/HeaderSearch/cabecalho";
+import { useSearchParams } from "next/navigation";
+import Perfil from "@/app/perfil/page";
+import Title from "./components/Title/title";
 
 
-export default function Home() {
-  return (
-    <>
-      <SignIn />
-    </>
-  )
-}
+const DashboardPage = () => {
+    const [programas, setProgramas] = useState<IPrograma[]>([]);
+    const { get } = useSearchParams();
+    const uuid = get("uuid");
+
+    const perfilId =
+        typeof window !== "undefined"
+            ? window.sessionStorage.getItem("perfilId")
+            : "";
+    console.log(`PerfilId é: ${perfilId}`);
+    const [userId, setUserId] = useState<string>("");
+    const [isOwner, setIsOwner] = useState<boolean>(false);
+    const [programasUsuario, setProgramasUsuario] = useState<IUsuarioPrograma[]>(
+        []
+    );
+
+    const fetchPerfil = async () => {
+        try {
+            const perfil = await ApiUtils.getByUuid<Perfil>(
+                "http://localhost:3333/usuario",
+                perfilId!
+            );
+            if (perfil) {
+                setUserId(perfil._id);
+                return perfil;
+            }
+            return null;
+        } catch (error) {
+            console.error("Erro ao obter o perfil:", error);
+            return null;
+        }
+    };
+
+    const fetchProgramasUsuario = async () => {
+        try {
+            const perfilData = await fetchPerfil();
+            if (perfilData) {
+                if (perfilData.perfil === "administrador") {
+                    // Se for administrador, retorna todos os programas
+                    const data = await ApiUtils.get<IPrograma[]>(
+                        "http://localhost:3333/programa"
+                    );
+                    return data || [];
+                } else {
+                    // Se não for administrador, busca os programas do usuário na tabela usuario-programa
+                    const programasUsuario = await ApiUtils.get<IUsuarioPrograma[]>(
+                        `http://localhost:3333/usuario-programa/${perfilData._id}/programas`
+                    );
+
+                    const programaIds = programasUsuario
+                        ? programasUsuario.map((programa) => programa.programaId)
+                        : [];
+
+                    // Busca os detalhes completos dos programas usando os IDs
+                    const programasCompletos = await Promise.all(
+                        programaIds.map(async (programaId) => {
+                            const programaCompleto = await ApiUtils.getByUuid<IPrograma>(
+                                "http://localhost:3333/programa",
+                                programaId
+                            );
+                            return programaCompleto || null;
+                        })
+                    );
+                    setIsOwner(true);
+                    return programasCompletos.filter(
+                        (programa) => programa !== null
+                    ) as IPrograma[];
+                }
+            }
+            return [];
+        } catch (error) {
+            console.error("Erro ao buscar os programas do usuário:", error);
+            return [];
+        }
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const programasUsuario = await fetchProgramasUsuario();
+            setProgramas(programasUsuario);
+        };
+
+        fetchData();
+    }, [perfilId]);
+
+    console.log(isOwner);
+
+    // Função auxiliar para verificar a permissão de edição/deleção para cada programa
+    const calculatePermission = (
+        programa: IPrograma,
+        userId: string,
+        programasUsuario: IUsuarioPrograma[]
+    ): boolean => {
+        // Verifica se o usuário é administrador
+        if (isOwner) {
+            return true;
+        }
+        // Verifica se o programa pertence ao usuário atual através da lista de programas do usuário
+        return programasUsuario.some(
+            (usuarioPrograma) =>
+                usuarioPrograma.programaId === programa._id &&
+                usuarioPrograma.usuarioId === userId
+        );
+    };
+
+    return (
+        <div className="flex w-screen">
+            <Sidebar />
+            <div className="flex flex-col">
+                <Cabecalho />
+                <div className="w-full bg-sky-200">
+                    <Title>Dashboard</Title>
+                    <div className="flex self-end p-4">
+                        <ButtonLinkPage href={`/programa/cadastrar`}>
+                            Nova solicitação +
+                        </ButtonLinkPage>
+                    </div>
+                    <main className="flex flex-wrap">
+                        {programas.map((programa) => (
+                            <CardProgram
+                                key={programa._id}
+                                programa={programa}
+                                userId={userId}
+                                isOwner={isOwner}
+                                hasPermission={isOwner}
+                            />
+                        ))}
+                    </main>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DashboardPage;
