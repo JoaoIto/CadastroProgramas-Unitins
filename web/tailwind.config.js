d7e3d717c22b122aa29acba005c8aedb4ebdf049
JoaoIto <joaovictorpfr@gmail.com>
2024-01-30 14:42:08 -0300

    novas cores e telas para config de estilizacoes

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,6 +6,28 @@
       './app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
+      screens: {
+        'sm': {'max': '760px'},
+        // => @media (min-width: 640px and max-width: 767px) { ... }
+
+        'md': {'min': '768px', 'max': '1023px'},
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+        'lg': {'min': '1024px'},
+        // => @media (min-width: 1024px and max-width: 1279px) { ... }
+      },
+      colors: {
+        transparent: 'transparent',
+        white: "#fff",
+        black: "#000",
+        cinzaClaro: "#F5F5F5",
+        cinzaTraco: "#ADADAD",
+        azulEscuro: "#00255B",
+        azulClaroGradient: "#40BAFF",
+        azulEscuroGradient: "#00255B",
+        verdeClaro: "#30D64C",
+        vermelho: "#CF0E0E"
+      },
       extend: {
         backgroundImage: {
           'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
