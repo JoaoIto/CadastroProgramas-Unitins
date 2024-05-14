e1a27ea57224725d93a07776ede09b988a8eefc9
João Victor <joaovictorpfr@gmail.com>
2024-05-14 15:16:59 -0300

    chore: novo programa interface

--- web/app/components/CardPrograma/Card.tsx
+++ web/app/components/CardPrograma/Card.tsx
@@ -1,64 +1,57 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import ButtonLinkPage from '@/app/components/ButtonLinkPage/ButtonLinkPage';
-import {ProgramaStatus} from "@/app/enum/programa-status.enum";
-import ApiUtils from '@/app/Utils/Api/apiMethods';
-import Perfil from "@/app/perfil/page";
 
 interface CardProgramProps {
-    programa: Programa;
+    programa: IPrograma;
 }
 
-interface Programa {
-    _id: string;
-    nomeCompleto: string;
-    rg: string;
-    cpf: string;
-    dataNascimento: string;
-    estadoCivil: string;
-    status?: string;
-}
-
-export const CardProgram: React.FC<CardProgramProps> = ({programa}) => {
-    const [perfil, setPerfil] = useState<string>('');
-
+export const CardProgram: React.FC<CardProgramProps> = ({ programa }) => {
     return (
         <Card
             key={programa._id}
             className="w-4/5 m-8 border-l-8 border-l-azulEscuroGradient shadow-md shadow-cinzaTraco rounded-2xl">
             <CardContent className="p-4">
                 <Typography variant="h5" component="div">
-                    {programa.nomeCompleto}
+                    {programa.titulo}
                 </Typography>
-                <Typography sx={{mb: 1.5}} color="text.secondary">
-                    RG: {programa.rg}
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    Usuário ID: {programa.usuarioId}
                 </Typography>
-                <Typography variant="body2">CPF: {programa.cpf}</Typography>
+                <Typography variant="body2">Descrição: {programa.descricao}</Typography>
+                <Typography variant="body2">Solução do Problema: {programa.solucaoProblemaDesc}</Typography>
+                <Typography variant="body2">Linguagens: {programa.linguagens.join(', ')}</Typography>
+                {programa.modificacaoTecnologicaDesc && (
+                    <Typography variant="body2">Modificação Tecnológica: {programa.modificacaoTecnologicaDesc}</Typography>
+                )}
+                <Typography variant="body2">Descrição do Mercado: {programa.descricaoMercado}</Typography>
                 <Typography variant="body2">
-                    Data de Nascimento: {programa.dataNascimento}
+                    Data de Criação do Programa: {new Date(programa.dataCriacaoPrograma).toLocaleDateString()}
                 </Typography>
-                <Typography variant="body2">Estado Civil: {programa.estadoCivil}</Typography>
+                <Typography variant="body2">
+                    Data de Criação: {new Date(programa.dataCriacao).toLocaleDateString()}
+                </Typography>
+                <Typography variant="body2">Vínculo com a Unitins: {programa.vinculoUnitins ? 'Sim' : 'Não'}</Typography>
+                {programa.vinculoInstitucional && (
+                    <Typography variant="body2">Vínculo Institucional: {programa.vinculoInstitucional}</Typography>
+                )}
+                <Typography variant="body2">Fase de Publicação: {programa.fasePublicacao}</Typography>
                 <Typography variant="body2">Status: {programa.status}</Typography>
+                {programa.nomeArquivo && (
+                    <Typography variant="body2">Nome do Arquivo: {programa.nomeArquivo}</Typography>
+                )}
                 <div className="flex gap-2 items-center">
                     <Button className="bg-azulEscuroGradient" variant="contained">
                         Visualizar
                     </Button>
                     <div>
                         <ButtonLinkPage href="/programa/editar" uuid={programa._id}>Editar</ButtonLinkPage>
-                    {/*    <button className="text-white bg-red-800 p-2 rounded font-Inter"*/}
-                    {/*            color="primary">*/}
-                    {/*            Deletar*/}
-                    {/*</button>*/}
+                    </div>
                 </div>
-                {/*<div>*/}
-                {/*    <button>Cancelar</button>*/}
-                {/*</div>*/}
-            </div>
-        </CardContent>
-</Card>
-)
-    ;
+            </CardContent>
+        </Card>
+    );
 };
