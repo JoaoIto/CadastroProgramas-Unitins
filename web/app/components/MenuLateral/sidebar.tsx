b8f353a6080eb1062d687f0feea1d1ca19d09452
João Victor <joaovictorpfr@gmail.com>
2024-06-13 16:43:16 -0300

    style: a full sidebar in a initial page

--- web/app/components/MenuLateral/sidebar.tsx
+++ web/app/components/MenuLateral/sidebar.tsx
@@ -1,35 +1,41 @@
 import React from "react";
 import Image from "next/image";
 import HomeIcon from "@mui/icons-material/Home";
-import ListIcon from '@mui/icons-material/List';
+import ListIcon from "@mui/icons-material/List";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { Button } from "@mui/material";
 
-export function Sidebar(){
-
-    return (
-        <aside
-            className={`h-full bg-azulEscuro text-white flex flex-col items-center flex-shrink-0 shadow-cinzaTraco drop-shadow-lg shadow-lg`}>
-            <ul className="flex flex-col items-center gap-10 mt-8 flex-1">
-                <li className="flex items-center space-x-2">
-                    <Button variant="text" className="text-white" href="/">
-                        <HomeIcon className="h-10 w-10" />
-                        <h3 className="sm:hidden font-medium text-lg p-4">Inicial</h3>
-                    </Button>
-                </li>
-                <li className="flex items-center space-x-2">
-                    <Button variant="text" className="text-white" href="/programa/listar">
-                        <ListIcon className="h-10 w-10" />
-                        <h3 className="sm:hidden font-medium text-lg p-4">Programas</h3>
-                    </Button>
-                </li>
-                <li className="flex items-center space-x-2">
-                    <Button variant="text" className="text-white" href="/perfil">
-                        <AccountCircleIcon className="h-10 w-10" />
-                        <h3 className="sm:hidden font-medium text-lg p-4">Perfil</h3>
-                    </Button>
-                </li>
-            </ul>
-        </aside>
-    );
-};
+export function Sidebar() {
+  return (
+    <div>
+      <aside
+        className={`h-full bg-azulEscuro text-white flex flex-col items-center flex-shrink-0 shadow-cinzaTraco drop-shadow-lg shadow-lg`}
+      >
+        <ul className="flex flex-col items-center gap-10 mt-8 flex-1">
+          <li className="flex items-center space-x-2">
+            <Button variant="text" className="text-white" href="/">
+              <HomeIcon className="h-10 w-10" />
+              <h3 className="sm:hidden font-medium text-lg p-4">Inicial</h3>
+            </Button>
+          </li>
+          <li className="flex items-center space-x-2">
+            <Button
+              variant="text"
+              className="text-white"
+              href="/programa/listar"
+            >
+              <ListIcon className="h-10 w-10" />
+              <h3 className="sm:hidden font-medium text-lg p-4">Programas</h3>
+            </Button>
+          </li>
+          <li className="flex items-center space-x-2">
+            <Button variant="text" className="text-white" href="/perfil">
+              <AccountCircleIcon className="h-10 w-10" />
+              <h3 className="sm:hidden font-medium text-lg p-4">Perfil</h3>
+            </Button>
+          </li>
+        </ul>
+      </aside>
+    </div>
+  );
+}
