7deecdf0851cc754bd346cdf7e09bc84c74c471b
João Victor <joaovictorpfr@gmail.com>
2024-07-22 11:00:55 -0300

    layout component sidebar

--- web/app/components/MenuLateral/sidebar.tsx
+++ web/app/components/MenuLateral/sidebar.tsx
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
 import HomeIcon from "@mui/icons-material/Home";
 import SendIcon from '@mui/icons-material/Send';
-import { Button } from "@mui/material";
+import { Button, List, ListItemButton, ListItemIcon, ListItemText, ListSubheader } from "@mui/material";
 import { useUserPayload } from "@/app/hooks/user/userPayload";
 import { useRouter } from "next/navigation";
 
 export function Sidebar() {
   const router = useRouter();
   const { profile, isLoading } = useUserPayload();
-  const [open, setOpen] = useState(false);
 
   if (isLoading) {
-    return <div>Loading...</div>; // Você pode substituir isso por um spinner ou qualquer outro indicador de carregamento
+    return <div>Loading...</div>;
   }
 
   const isAdmin = profile.perfil === "admin";
 
   const routerProgramas = () => {
-    router.push('/')
+    router.push('/programa/listar')
   };
 
   const routerDashboard = () => {
@@ -26,22 +25,35 @@ export function Sidebar() {
 
   return (
     <div>
-      <aside className="sticky top-0 left-0 h-[250px] bg-azulEscuro py-12 px-6 rounded-e-3xl shadow-lg shadow-cinzaTraco w-64">
-        <nav className="text-white">
-          <ul className="flex flex-col gap-4">
-            <li>
-              <Button onClick={routerDashboard} className="flex items-center space-x-2 text-white">
-                <HomeIcon />
-                <span className="text-md">Inicial</span>
-              </Button>
-            </li>
-            <li>
-              <Button onClick={routerProgramas} className="flex items-center space-x-2 text-white">
-                <SendIcon />
-                <span className="text-md">Programas</span>
-              </Button>
-            </li>
-          </ul>
+      <aside className="sticky top-20 left-0 h-[250px] bg-azulEscuro py-12 px-6 rounded-e-3xl shadow-lg shadow-cinzaTraco w-64">
+        <nav aria-labelledby="menu-sidebar">
+          <List
+            sx={{ width: '100%', maxWidth: 360 }}
+            className="bg-azulEscuro text-[#D0D0D0] min-w-1/4"
+            component="nav"
+            subheader={
+              <ListSubheader
+                component="div"
+                id="menu-sidebar"
+                className="bg-azulEscuro text-white"
+              >
+                Menu Lateral
+              </ListSubheader>
+            }
+          >
+            <ListItemButton onClick={routerDashboard}>
+              <ListItemIcon>
+                <HomeIcon sx={{ color: 'white' }} />
+              </ListItemIcon>
+              <ListItemText primary="Inicial" />
+            </ListItemButton>
+            <ListItemButton onClick={routerProgramas}>
+              <ListItemIcon>
+                <SendIcon sx={{ color: 'white' }} />
+              </ListItemIcon>
+              <ListItemText primary="Programas" />
+            </ListItemButton>
+          </List>
         </nav>
       </aside>
     </div>
